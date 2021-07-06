b0044a623e3af33ec29e546d9f0044b6046a1379
Mert <mertrizakaradeniz@gmail.com>
2021-07-06 11:21:44 +0300

    Display tv shows

---
+++ app/src/main/java/com/example/tvshows/adapter/TVShowsAdapter.java
@@ -0,0 +1,71 @@
+package com.example.tvshows.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tvshows.data.model.TVShow;
+import com.example.tvshows.databinding.ItemContainerTvShowBinding;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class TVShowsAdapter extends RecyclerView.Adapter<TVShowsAdapter.TVShowViewHolder> {
+
+    private List<TVShow> tvShows;
+
+    public TVShowsAdapter(List<TVShow> tvShows) {
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public TVShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TVShowViewHolder(ItemContainerTvShowBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TVShowsAdapter.TVShowViewHolder holder, int position) {
+        TVShow tvShow = tvShows.get(position);
+        RoundedImageView imageView = holder.itemContainerTvShowBinding.imgTVShow;
+        imageView.setAlpha(0f);
+        Picasso.get().load(tvShow.getImageThumbnailPath()).noFade().into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                imageView.animate().setDuration(300).alpha(1f).start();
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+        holder.itemContainerTvShowBinding.tvName.setText(tvShow.getName());
+        holder.itemContainerTvShowBinding.tvNetwork.setText(tvShow.getNetwork() + " (" + tvShow.getCountry() + ")");
+        holder.itemContainerTvShowBinding.tvStarted.setText("Started on: " + tvShow.getStartDate());
+        holder.itemContainerTvShowBinding.tvStatus.setText(tvShow.getStatus());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    static class TVShowViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemContainerTvShowBinding itemContainerTvShowBinding;
+
+        public TVShowViewHolder(ItemContainerTvShowBinding itemContainerTvShowBinding) {
+            super(itemContainerTvShowBinding.getRoot());
+            this.itemContainerTvShowBinding = itemContainerTvShowBinding;
+        }
+    }
+}
--- app/src/main/java/com/example/tvshows/ui/main/MainActivity.java
+++ app/src/main/java/com/example/tvshows/ui/main/MainActivity.java
@@ -4,28 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
 
-import com.example.tvshows.R;
+import com.example.tvshows.adapter.TVShowsAdapter;
+import com.example.tvshows.data.model.TVShow;
+import com.example.tvshows.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     private MostPopularTVShowsViewModel viewModel;
+    private List<TVShow> tvShows = new ArrayList<>();
+    private TVShowsAdapter tvShowsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        doInitialization();
+    }
+
+    private void doInitialization() {
+        binding.rvTvShows.setHasFixedSize(true);
         viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        tvShowsAdapter = new TVShowsAdapter(tvShows);
+        binding.rvTvShows.setAdapter(tvShowsAdapter);
         getMostPopularTVShows();
+//        Log.d("SAAS", "" + tvShows.size());
     }
 
     private void getMostPopularTVShows() {
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Total Pages: " + mostPopularTVShowsResponse.getPages(),
-                        Toast.LENGTH_SHORT).show()
-        );
+        binding.pbLoading.setVisibility(View.VISIBLE);
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> {
+            binding.pbLoading.setVisibility(View.GONE);
+            if (mostPopularTVShowsResponse != null) {
+                if (mostPopularTVShowsResponse.getTvShows() != null) {
+                    tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
+//                    Log.d("SAAS", "" + tvShows.size());
+                    tvShowsAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
